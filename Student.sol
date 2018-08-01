b71037f7d85eafb172f229a22188a4eaba00d565
balachandra87 <balachandrapulavarthi@gmail.com>
2018-08-01 07:28:00 +0530

    updating student contract

--- Student.sol
+++ Student.sol
@@ -1,4 +1,33 @@
-contract StructMap {
+contract Owanble {
+    
+    address public admin;
+    
+    constructor() public {
+        admin = msg.sender;
+    }
+    
+    modifier onlyAdmin () {
+        require(msg.sender == admin);
+        _;
+    }
+}
+
+contract TimeBased {
+    uint256 public freezeTimeStamp;
+    
+    constructor () public {
+        freezeTimeStamp = now +30;
+    }
+    
+    modifier onlyAllowedTime () {
+        require(now<= freezeTimeStamp);
+        _;
+    }
+}
+
+
+
+contract StructMap is Owanble,TimeBased {
     
     enum results {failed,absent,passed}
     
@@ -9,9 +38,19 @@ contract StructMap {
         results examResult;
     }
     
+    
+    event SudnetAdded(address _studentAddress,address indexed _cretedBy,uint256 timeStamp);
+    
+      
     mapping (address => Student) studentDetails;
     
-    function addStudent(address _studentAddress,uint8 _rollno,string _name, uint8 _marks) public {
+    
+    constructor() public  {
+         freezeTimeStamp = now+30;
+    }
+
+    function addStudent(address _studentAddress,uint8 _rollno,string _name, uint8 _marks) public onlyAdmin onlyAllowedTime {
+        
         
         if(_marks >35)
         {
@@ -20,9 +59,11 @@ contract StructMap {
         else {
             studentDetails[_studentAddress] = Student(_rollno,_name,_marks,results.failed);
         }
+        
+        emit SudnetAdded(_studentAddress,msg.sender,now);
     }
     
-    function getStudentDetails(address _studentAddress) public view returns(uint8,string,uint8,results){
+    function getStudentDetails(address _studentAddress) public view onlyAdmin returns(uint8,string,uint8,results) {
         
         Student memory temp = studentDetails[_studentAddress];
         
